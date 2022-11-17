a38909b617b867267d4fcbc9ecbcff6fd11244ce
AngeloDiMortimer <angelodimortimer99@gmail.com>
2022-11-17 17:39:28 -0400

    Made createGrid function, working as intended

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+const resetButton = document.querySelector('button');
+const container = document.querySelector('.container');
+
+let sizeOfGrid = 16;
+
+const createGrid = (numbOfGrids) => { //Creates an specific number of grids as needed. I.E: A 16x16 grid of square divs.
+    
+    for (let i = 0; i < numbOfGrids; i++) {//makes the rows where the gridboxes will be
+        const row = document.createElement('div');
+        row.classList.add('grid-row');
+
+        for (let j = 0; j < numbOfGrids; j++) { //makes the gridboxes
+            const gridBox = document.createElement('div');
+            gridBox.classList.add('grid-box');
+            row.appendChild(gridBox);
+        }
+
+        container.appendChild(row);
+    }
+}
+
+createGrid(sizeOfGrid);
