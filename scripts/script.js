28aee05169d2b5c181efc06279a90b45ea5e2f34
AngeloDiMortimer <angelodimortimer99@gmail.com>
2022-11-18 17:25:34 -0400

    Changed grid size to be more dynamic

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,7 @@ let sizeOfGrid = 16;
 const createGrid = (numbOfGrids) => { //Creates an specific number of grids as needed. I.E: A 16x16 grid of square divs.
     
     for (let i = 0; i < numbOfGrids; i++) {//makes the rows where the gridboxes will be
-        const widthAndHeight = 960 / sizeOfGrid; //size of the container
+        const widthAndHeight = 660 / sizeOfGrid; //size of the container
         const row = document.createElement('div');
         row.classList.add('grid-row');
 
